a02b0599edfba1e1ebf5fa2f5eb530aeac49cc39
bulter1996 <18219114498@163.com>
2020-09-03 18:13:14 +0800

    20% ...

--- temp.py
+++ temp.py
@@ -1,1 +1,16 @@
-print("HelloWorld")
+{
+    'hight': {
+        1: {
+            'look': {
+                1: {
+                    'income':
+                         {1:
+                              {'shape':
+                                   {1: 1, 2: 1}
+                               },
+                          2: 1,
+                          3: {'shape': {1: 1, 2: 0}}}}, 2: 1,
+                        3: {'income': {1: 1, 2: 0}}}}, 2: {'income': {1: 1, 2: {'look': {1: 1, 2: 0}}, 3: 0}},
+           3: {'look': {1: {'shape': {3: 0, 1: 1}}, 2: 0, 3: 0}}}
+}
+url = 'https://blog.csdn.net/weixin_40683253/article/details/81941583?utm_medium=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param&depth_1-utm_source=distribute.pc_relevant.none-task-blog-BlogCommendFromMachineLearnPai2-1.channel_param#%E6%BA%90%E6%95%B0%E6%8D%AE'
